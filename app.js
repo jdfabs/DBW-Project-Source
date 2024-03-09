c78f42110b86c04cac909dbcc68c102b4c173b1f
jdfabs <67124317+jdfabs@users.noreply.github.com>
2024-03-09 22:32:35 +0000

    001

--- app.js
+++ app.js
@@ -26,12 +26,15 @@ app.use(morgan("dev"));
 
 
 app.get("/getResponse", async (req, res) =>{
-  const response = await openai.chat.completions.create({
-      model:"gpt-3.5-turbo",
-      messages:[{"role":"user","content":"essay on global warming"}],
-      max_tokens:100
-  })  
-  console.log(response.choices[0]);
+
+const response = await openai.chat.completions.create({
+  model:"gpt-3.5-turbo",
+  messages:[{"role":"user","content":"in any recepie you may freely include any basic spice./n Be extremely shot and precise on each section of the answer, respond only the the following template, without any excuses, '--' means a comment for you:/n/n Name: [RECEPIE NAME]/n/n Description Short: -- maximum 25 words/n/n Ingredients: [INGREDIENT 1], [INGREDIENT  2], .../n Preparation time: [TIME]/n Cooking time: [TIME]/n Difficulty: [DIFFICULTY] -- out of 10/n Portions: [NUMBER + UNIT]/n/n PREPARATION: -- create these steps for begginners, minimum 20 steps/n 1 - [STEP 1] /n 2 - [STEP 2] /n ..."},
+{"role":"user","content":"generate me a recepie using these ingredients:/n colored bell peppers, chicken, cream cheese, dry ranch dressing,  cheddar cheese, bacon /n You may remove up to two of the previous ingredients then add two more new ingredients./n Using an oven, maximum 1h30m, easy dificulty"}],
+  max_tokens:400
+})  
+console.log(response.choices[0]);
+
 })
 
 
@@ -109,22 +112,7 @@ app.get("/recepie", (req, res) => {
 });
 app.get("/recepieGenerator", (req, res) => {
   const recepies = [
-    {
-      title: "Recepie 1",
-      description: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Recepie 2",
-      description: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Recepie 3",
-      description: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Recepie 4",
-      description: "Lorem ipsum dolor sit amet consectetur",
-    },
+ 
   ];
   res.render("recepieGenerator", { title: "Recepie Generator", recepies });
 });
