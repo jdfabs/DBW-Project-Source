a0743f2a7619c876aa14c334aa7f2101818714cc
jdfabs <67124317+jdfabs@users.noreply.github.com>
2024-03-15 13:28:11 +0000

    main page js update - adicionar mais ingredientes ou methodos dinamicamente
    falta retirar

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,5 +1,7 @@
 const filterButton = document.getElementById("filterButton");
 const filterWindow = document.getElementById("filterWindow");
+const methodList = document.getElementById("methodList");
+const ingredientList = document.getElementById("ingredientList");
 
 filterButton.addEventListener("click", (event) => {
   console.log("filterButton clicked");
@@ -10,18 +12,29 @@ filterButton.addEventListener("click", (event) => {
 });
 
 const addMethodButton = document.getElementById("moreMethod");
-
 const addIngredientButton = document.getElementById("moreIng");
 
 addMethodButton.addEventListener("click", (event) => {
   event.preventDefault();
   const newMethod = document.createElement("div");
+  newMethod.classList.add("row");
+  newMethod.innerHTML = `<select itemid="methods" class="col-10" name="cookingMethods" id="cookingMethods">
+  <option value="Method 1">Method 1</option>
+  <option value="Method 2">Method 2</option>
+  <option value="Method 3">Method 3</option>
+  <option value="Method 4">Method 4</option>
 
-  newMethod.innerHTML = `<select itemid="methodList" class="input-sm" name="cookingMethods" id="cookingMethods">
-      <option value="Method 1">Method 1</option>
-      <option value="Method 2">Method 2</option>
-      <option value="Method 3">Method 3</option>
-      <option value="Method 4">Method 4</option>
+</select>
+<button class="removeBtn">-</button>`;
+    methodList.append(newMethod);
 
-    </select>`;
+});
+
+addIngredientButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const newIngredient = document.createElement("div");
+  newIngredient.classList.add("row");
+  newIngredient.innerHTML = `<input class="col-10" name="Ingredient" type="text" placeholder="Ingredient" />
+  <button class="removeBtn col-2">-</button>`;
+  ingredientList.append(newIngredient);
 });
