70c235051b6f8ea9740199193b8ac7162c83a5dd
jdfabs <67124317+jdfabs@users.noreply.github.com>
2024-04-21 16:39:54 +0100

    Update dbValidation.js

--- middlewares/dbValidation.js
+++ middlewares/dbValidation.js
@@ -3,7 +3,6 @@ const recipeModel = require("../model/recipeModel");
 const userModel = require("../model/userModel");
 const debug = require("../debugTools");
 
-const CYCLE_LIMIT = 5;
 
 const isRecipeValid = async function (base) {
   debug.log(1, "validateRecipe:");
@@ -25,46 +24,5 @@ const isUserValid = async function (user) {
     return false;
   }
 };
-
-/*
-const forceValidRecipe = async function (base) {
-  debug.log(1, "forceValidRecipe:");
-  let validRecipe = base;
-  let isFixed = false;
-  let counter = 0;
-  console.log(base);
-  while (!isFixed && counter < CYCLE_LIMIT) {
-    console.log("Loop num: " + counter);
-    try {
-      await recipeModel.validate(validRecipe);
-      return validRecipe;
-    } catch (error) {
-      try {
-        const missingParams = Object.keys(error.errors);
-        console.log(Object.keys(error.errors));
-        //Por cada campo em falta
-        for (let param of missingParams) {
-          // fixData return = valid JSON - recipe com este parametro arranjado
-          validRecipe = await fixer.fixData(param, validRecipe);
-        }
-      } catch {
-        validRecipe = await fixer.fixData(
-          String(error).substring(
-            String(error).indexOf("'") + 1,
-            String(error).indexOf("')")
-          ),
-          validRecipe
-        );
-      }
-    }
-    console.log("Loop: " + counter + " result:");
-    console.log(validRecipe);
-    counter++;
-  }
-  if (!isFixed) {
-    console.log("@@@@@@@@@ FATAL ERROR - FAILED TO FORCE RECIPE VALIDATION");
-  }
-  return validRecipe;
-};*/
 
 module.exports = { isRecipeValid, isUserValid };
