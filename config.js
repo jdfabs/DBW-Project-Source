86551a2f541ad3098c484a98f55ad317e3cf891d
andre2387 <161973263+andre2387@users.noreply.github.com>
2024-04-28 18:53:52 +0100

    Revert "gg28"
    This reverts commit d99237c066759ad54fe9189ae36488054cec12c1.

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ const config = {
    devMode: true,
    port: 3000,
    dbUrlMongoDb: "tbd",
-   openAI_API_Key: "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",
-   dbPassword: "Andredbw1",
-   dbUser: "Andredbw1",
-   mockData: true,
-   sessionKey: "1234qwer=)(/asdfzxcv",
+   openAI_API_Key: "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"
 };
 
 module.exports = config;
