748c211fe23b82151f36115e63abdbf3a3d22214
jdfabs <67124317+jdfabs@users.noreply.github.com>
2024-04-30 21:13:35 +0100

    recipe page - livro agora pode receber receitas de qualquer tamanho!

--- controllers/authenticated/recepie.js
+++ controllers/authenticated/recepie.js
@@ -5,7 +5,17 @@ const recipeGet = async function (req, res) {
   const result = await dbManager.getRandomRecipe();
   const recipe = result[0];
 
-  res.render("recipe", { title: "Recipe", recipe ,  isAuthenticated: req.body.isAuthenticated });
+
+  let isOwner;
+  if (recipe.creator == req.user) isOwner = true;
+  else isOwner = false;
+
+  res.render("recipe", {
+    title: "Recipe",
+    recipe,
+    isAuthenticated: req.body.isAuthenticated,
+    isOwner
+  });
 };
 
 const recipeIdGet = async function (req, res) {
@@ -13,16 +23,34 @@ const recipeIdGet = async function (req, res) {
     const recipeId = req.params.id;
     // Fetch the recipe details from the database using the recipeId
     const recipe = await dbManager.getRecipeById(recipeId);
-    console.log(recipe);
     if (!recipe) {
       // If the recipe with the given ID is not found, render an error page
-      return res.status(404).render("404", { title: "Not Found" ,  isAuthenticated: req.body.isAuthenticated });
+      return res
+        .status(404)
+        .render("404", {
+          title: "Not Found",
+          isAuthenticated: req.body.isAuthenticated,
+        });
     }
+
+    let isOwner;
+    if (recipe.creator == req.user.username) isOwner = true;
+    else isOwner = false;
+
     // Render a page to display the recipe details
-    res.render("recipe", { title: "Recipe", recipe ,  isAuthenticated: req.body.isAuthenticated });
+    res.render("recipe", {
+      title: "Recipe",
+      recipe,
+      isAuthenticated: req.body.isAuthenticated,isOwner
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).render("404", { title: "Internal Server Error" ,  isAuthenticated: req.body.isAuthenticated });
+    res
+      .status(500)
+      .render("404", {
+        title: "Internal Server Error",
+        isAuthenticated: req.body.isAuthenticated,
+      });
   }
 };
 module.exports = { recipeGet, recipeIdGet };
