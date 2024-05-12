e244a0883b89a46282c877b5e2fb52301a604ea6
jdfabs <67124317+jdfabs@users.noreply.github.com>
2024-05-12 16:46:36 +0100

    Revert "mainPage Search cleanUp"
    This reverts commit fff18c8ee392c0845f0bbbe62bdc80b039c6a43f.

--- controllers/authenticated/mainPage.js
+++ controllers/authenticated/mainPage.js
@@ -18,9 +18,7 @@ const getTopThreeRecipes = async function (filters) {
 };
 
 const getRecipeByIndex = async function (filters, index) {
-  let filterArray = [];
-  if(filters.searchBar) filterArray = filters.searchBar.split(" ");
-  
+  let filterArray = filters.searchBar.split(" ");
 
   filters.ingredients.forEach((filter) => {
     filterArray.push(filter);
@@ -28,11 +26,11 @@ const getRecipeByIndex = async function (filters, index) {
   filters.methods.forEach((filter) => {
     filterArray.push(filter);
   });
-  console.log(filterArray);
-  let filterQueries = filterArray.map((filter) => {
+
+  const filterQueries = filterArray.map((filter) => {
     const regex = new RegExp(filter.trim(), "i");
     return {
-      $and: [
+      $or: [
         { recipeName: { $regex: regex } },
         { ingredients: { $regex: regex } },
         { tags: { $regex: regex } },
@@ -41,36 +39,12 @@ const getRecipeByIndex = async function (filters, index) {
   });
 
   // Combine individual filter queries using logical OR
-  let combinedQuery = { $or: filterQueries };
+  const combinedQuery = { $or: filterQueries };
 
   try {
     // Search the database for recipes matching any of the filters
-    let recipe = await recipeModel.find(combinedQuery).sort("asc").skip(index).limit(1);
-    console.log(recipe)
-    if(recipe[0]) return recipe[0];
-    console.log("No more exact matches");
-    try{
-      filterQueries = filterArray.map((filter) => {
-        const regex = new RegExp(filter.trim(), "i");
-        return {
-          $or: [
-            { recipeName: { $regex: regex } },
-            { ingredients: { $regex: regex } },
-            { tags: { $regex: regex } },
-          ],
-        };
-  
-      });
-      combinedQuery = { $or: filterQueries };
-      recipe = await recipeModel.find(combinedQuery).sort("asc").skip(index).limit(1);
-      return recipe[0];
-
-    }
-    catch (error){
-console.log(error);
-    }
-     
-
+    const recipe = await recipeModel.find(combinedQuery).skip(index).limit(1);
+    return recipe[0];
   } catch (error) {
     console.error("Error while fetching recipes:", error);
     throw error;
